69d5674164aa50cf21f27b6b8fe5b11980c7ceb1
Farber98 <juantuc98@gmail.com>
2022-03-12 20:47:43 -0300

    feat: basic auth

--- cmd/go-echo-server/router.go
+++ cmd/go-echo-server/router.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/subtle"
+	"net/http"
+
 	controllers "github.com/Farber98/go-echo-server/internal/controllers"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -16,6 +19,14 @@ func initRoutes(r *echo.Echo) {
 	adminGroup := r.Group("/admin")
 	adminGroup.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: `[${time_rfc3339}] ${status} ${method} ${host} ${path} ${latency_human}` + "\n",
+	}))
+
+	adminGroup.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if subtle.ConstantTimeCompare([]byte(username), []byte("Jac")) == 1 &&
+			subtle.ConstantTimeCompare([]byte(password), []byte("1234")) == 1 {
+			return true, nil
+		}
+		return false, c.JSON(http.StatusUnauthorized, struct{ Mensaje string }{"Not authorized"})
 	}))
 
 	adminGroup.GET("/main", controllers.MainAdmin)
